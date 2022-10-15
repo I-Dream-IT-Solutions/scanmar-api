68c105bb88d926516cdb0055ff437e290aff7b5a
Harvey Mendoza <harvzhack@Harveys-MacBook-Pro.local>
2022-10-15 23:31:46 +0800

    CrewContact adding filter and retest api

--- app/Action/CrewContact/CrewContactListAction.php
+++ app/Action/CrewContact/CrewContactListAction.php
@@ -14,7 +14,13 @@ class CrewContactListAction
 
   public function execute($request)
   {
+
     $records = new CrewContact();
+
+    $records = $records->where('crew_no',Auth::user()->crew_no);
+    if($request->has('search'))
+    $records = $records->where('label','like','%'.$request->get('search').'%')
+      ->orWhere('description','like','%'.$request->get('search').'%');
 
     $records = $records->orderBy('id','ASC');
     if($request->has('limit'))
@@ -25,7 +31,5 @@ class CrewContactListAction
     return $records;
 
   }
-
-
 
 }
